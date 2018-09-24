409298d08aff536d5fdc060187c565ef7996697d
Inal Kardanov <kardinal3094@gmail.com>
2018-09-24 22:45:00 +0300

    Fixes an issue with swagger crash #1 https://github.com/wavesplatform/node-docker-image/issues/1

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 /usr/bin/python3 "/waves-node/starter.py"
 echo "Node is starting..."
 mkdir -p /waves/data
-/usr/bin/java -Dlogback.stdout.level="${WAVES_LOG_LEVEL}" "-Xmx${WAVES_HEAP_SIZE}" -jar "/waves-node/waves-all-${WAVES_VERSION}.jar" $WAVES_CONFIG_FILE
+/usr/bin/java --add-modules=java.xml.bind --add-exports java.base/jdk.internal.ref=ALL-UNNAMED -Dlogback.stdout.level="${WAVES_LOG_LEVEL}" "-Xmx${WAVES_HEAP_SIZE}" -jar "/waves-node/waves-all-${WAVES_VERSION}.jar" $WAVES_CONFIG_FILE
